b0d86f17bb7d68eeb9dbaca93c179b0af9880718
izypsy <reddy.nie11@gmail.com>
2024-04-17 02:28:37 +0530

    fetch ssh_secrets & app_secrets

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e role_name=$component -e env=$env
-ansible-playbook -i $component-$env.izypsy.cloud, -e '@ssh_secrets.json' -e '@app-secrets.json' -e role_name=$component -e env=$env expense-setup.yml
+ansible-playbook -i $component-$env.izypsy.cloud, -e '@ssh-secrets.json' -e '@app-secrets.json' -e role_name=$component -e env=$env expense-setup.yml
