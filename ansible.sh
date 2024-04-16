683f7394027b0bbacd1f3dab0b9c3646383f31bf
izypsy <reddy.nie11@gmail.com>
2024-04-17 01:10:35 +0530

    dns update in ansible.sh file

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,3 @@
 component=$1
-host=$2
-ip=$3
-ansible-playbook -i $host, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$component -e env=$env expense-setup.yml
+
+ansible-playbook -i $component-$env.izypsy.cloud, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$component -e env=$env expense-setup.yml
