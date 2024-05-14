3c4ae47552a845f6a9f97a0de342d0dbdf13deb0
izypsy <reddy.nie11@gmail.com>
2024-05-14 18:22:29 +0530

    update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,5 @@
 component=$1
+rm -f ~/ssh-secrets.json ~/app-secrets.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e role_name=$component -e env=$env
 ansible-playbook -i $component-$env.izypsy.cloud, -e '@~/ssh-secrets.json' -e '@~/app-secrets.json' -e role_name=$component -e env=$env expense-setup.yml
-rm -f ~/ssh-secrets.json ~/app-secrets.json
+#rm -f ~/ssh-secrets.json ~/app-secrets.json
